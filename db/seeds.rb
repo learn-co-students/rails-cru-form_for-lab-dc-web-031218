5b02a7eed465f8819189b8709f9ef3007aab37b2
rpyoungdale <youngdale95@gmail.com>
2018-04-10 09:21:58 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+noah_gundersen = Artist.create(name: "Noah Gundersen", bio: "Dark, sad tunes.")
+dermot_kennedy = Artist.create(name: "Dermot Kennedy", bio: "Edgy, chill tunes.")
+acoustic = Genre.create(name: "Acoustic")
+alternative = Genre.create(name: "Alternative")
+first_defeat = Song.create(name: "First Defeat")
+a_closeness = Song.create(name: "A closeness")
